fc2b055e6b64b40211b07a1924e78df7672856f9
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-02-04 22:03:41 +0500

    Add files via upload
    Done

---
+++ pr_03.js
@@ -0,0 +1,131 @@
+//  TEMPLATE LITERALL
+
+
+// function table(a) 
+// {
+//     for (let i = 0; i <= 10; i++) {
+//         console.log(`${a} X ${i} = ${a*i}`);
+        
+//     }
+    
+// }
+
+
+
+// table(4);
+
+
+
+
+//  DEFAULT PARAMETER
+
+
+
+// function Val(a,b=4) 
+// {
+//     return console.log(a+b); 
+// }
+
+// Val(3);
+
+
+
+//  ARRAY
+
+// var arr = ['Mabtoor', 'Waqas', 'Horair', 'Waheed'];
+// console.log(arr);
+
+
+
+// FAT   Arrow Function
+
+
+// const sum = (a,b) =>
+// {
+//     return console.log(a+b);
+// }
+
+
+// sum(2,3);
+
+
+
+//  Traversal Array
+
+
+// var arr = ["Mabtoor", "Waqas", "Horair", "Waheed"];
+// console.log(arr.length);
+// console.log(arr[2]);
+
+
+
+//  For Of Loop
+
+
+// let arr = ["Mabtoor", "Waqas", "Waheed", "Horair"];
+// for (const elements of arr) 
+// {
+//     console.log(elements);
+    
+// }
+
+
+
+
+//  For In Loop
+
+
+// let arr = ["Orange", "Mango", "Pineapple", "Vanilla"];
+
+// for (const elements in arr) 
+//      {
+//         console.log(elements);
+        
+//     }
+
+
+
+
+//  FOR EACH LOOP
+
+
+
+let arr = ["Mabtoor", "Waqas", "Waheed", "Horair"];
+
+arr.forEach(function(element,index,array)
+{
+console.log(`${element} , ${index}, ${array}`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
