2bb31ce0f4d8006a114f763b4cb61a92d6b7caf3
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-02-04 12:06:45 +0500

    Add files via upload

---
+++ pr_01.js
@@ -0,0 +1,17 @@
+// var a = 72;
+// var b = 43;
+// if (a>=b) 
+// {
+//     console.log("A is Greater Than B");
+    
+// }
+// else
+// {
+//     console.log("A is Lesser Than B");
+// }
+
+
+// for(let i = 0; i < 100; i++) {
+//     console.log(i);
+    
+// }
---
+++ pr_02.js
@@ -0,0 +1,88 @@
+
+//  FUNCTION IN JS
+
+// function Add(a,b) 
+// {
+//     return a+b;
+// }
+
+// function Sub(a,b) 
+// {
+//     return a-b;
+    
+// }
+// function Mult(a,b) 
+// {
+//     return a*b;
+// }
+// function Div(a,b) 
+// {
+//     return a/b;
+    
+// }
+
+
+
+// var a= 10;
+// var b = 12;
+// var op = "-";
+
+// if (op=="+") 
+// {
+//  console.log(Add(a,b));   
+// }
+// else if (op=="-") 
+// {
+//  console.log( Sub(a,b) );    
+// }
+
+// else if (op=="*") 
+// {
+//  console.log(Mult(a,b));
+// }
+
+// else if (op=="/") 
+// {
+//  console.log(Div(a,b)); 
+// }
+// else
+// {
+//     console.log("Invalid Operator");
+// }
+
+
+
+
+//  LET AND CONST
+
+
+
+// let a  = 10;
+// let a = 12;
+// console.log(a);
+
+// var a= 12;
+// var a = 123;
+// console.log(a);
+
+
+// const a= 12;
+// const a= 11;
+// console.log(a);
+
+
+
+
+
+//  ANONYMOUS FUNCTION
+
+
+// let add = function(a,b)
+// {
+//     return a+b;
+// }
+
+
+// console.log(add(3,4));
+
+
