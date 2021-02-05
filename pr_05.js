7567e36cad8961255f7b8ebed589643339714050
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-02-05 14:30:56 +0500

    Add files via upload
    done

---
+++ pr_05.js
@@ -0,0 +1,65 @@
+
+// let arr = ["Rishab","Pandit", "Munna", "Harshad", "Akkahanda", "Guddu", "bablu"];
+// console.log(arr[2]);
+
+
+
+//  SEARCH METHOD IN ARRAY
+
+
+// console.log(arr.indexOf("Harshad"));
+
+// console.log(arr.lastIndexOf("Harshad", 3));
+
+// console.log(arr.includes("Munna"));
+
+
+
+
+
+//  FIND METHOD IN ARRAY
+
+// let arr = [21,2,423,12,1,3424,,23,1,,13,1,3213,1,1];
+
+// let finde = arr.find(function(element)
+// {
+//     return element >3
+// });
+
+
+// console.log(finde);
+
+
+
+// let index = arr.findIndex(function(element)
+// {
+//     return element > 24;
+// });
+
+// console.log(index);
+
+
+// let filt = arr.filter(function(element){
+//     return element > 20;
+// });
+// console.log(filt);
+
+
+
+// let filte = arr.filter(element=>{
+//     return element > 21;
+// });
+// console.log(filte);
+
+
+
+//  SORT AND COMPARE
+
+
+// let arr = [12,1000000, 1365,231];
+
+// console.log(arr.sort());
+
+
+
+
