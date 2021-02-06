75253f06a42adf49be250cfd2e8ce32c33e131d8
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-02-06 12:57:17 +0500

    Add files via upload

---
+++ pr_06.js
@@ -0,0 +1,35 @@
+//  MAP AND REDUCE
+
+
+// let arr = [21,3,1,4,2,1,3,4,4];
+
+// let mapp =  arr.map((element,index,array)=>
+// { return element > 2 });
+
+// console.log(mapp);
+
+
+
+// let arr = [16,25,36,49,64,81]
+
+// let sqrt = arr.map((element,) => { return Math.sqrt(element) });
+// console.log(sqrt);
+
+
+
+
+
+
+//  MAP + REDUCE + FILTER IN THIS EXAMPLE
+// let arr = [1,3,4,5,6,7,8,9]
+
+
+// let sq = arr.map((element)=>{return element*2}).filter((element)=>{return element>10}).reduce((accumulator,element)=>{return accumulator+=element});
+// console.log(sq);
+
+
+
+
+
+
+
