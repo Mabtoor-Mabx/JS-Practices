a2567bd76d36ba02cc1a86c07180fd9679b8d9c4
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-03-01 15:12:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+
+//  BASIC OVERVIEW 
+
+
+// alert("Hello World");
+// console.log("Hello World");
+// // console.clear();
+// console.warn("This Is Warning");
+// console.error("This Is Error");
+// console.assert(3==4);
+// document.write("Hello World");
+
+
+
+//  OBJECTS 
+
+
+// let marks = {
+//     Ali: 76,
+//     Umer : 93,
+//     Asfand : 84,
+//     Pervaiz : 86
+// }
+
+
+// console.log(marks);
+// let arr = [21,13,241,1,4,1,2,14,1,2,1];
+// console.log(arr.sort());
+
+
+
+//  CALCULATOR IN JS
+
+
+// function Add(a,b) 
+// {
+//    console.log(a+b);   
+// }
+
+// function Sub(a,b) 
+// {
+//     console.log(a-b);   
+// }
+
+// function Mult(a,b) 
+// {
+//     console.log(a*b);   
+// }
+
+
+// function Div(a,b) 
+// {
+//     console.log(a/b);   
+// }
+
+// let val1 = Number(prompt("Enter First Value"));
+// let val2 = Number(prompt("Enter Second Value")); 
+//  let op = prompt("Enter The Operator");
+
+// if (op=="+") {
+//     Add(val1,val2)
+// }
+// else if (op=="-") {
+//     Sub(val1,val2)
+// }
+// else if (op=="*") {
+//     Mult(val1,val2)
+// }
+// else if (op=="/") {
+//     Div(val1,val2)
+// }
+
+
+
+//  ARRAY METHODS
+
+let arr = ['Ali', 'Asfand', 'Ahmad', 'Ahmer', 'Asgher', 'Amjad', 'Azad', 'Azadi', 'Asifa', 'Anjum'];
+
+// let splice = arr.splice(2,0,'Alizay');
+// let push = arr.push("Mabtoor");
+// let pop = arr.pop();
+// let shift = arr.shift();
+// let unshift = arr.unshift("Mabx");
+// let slice = arr.slice(1,4);
+// console.log(slice);
+// let toString = arr.toString();
+// console.log(toString);
+// let length = arr.length;
+// console.log(length);
+// let sort = arr.sort();
+// console.log(sort);
+// arr[4]= "Mabx";
+// let concat = arr.concat("GOOD");
+// console.log(concat);
+// console.log(arr);
+
+
+
+
+
+//  LOOPS
+
+
+// arr.forEach(function(e){console.log(e);})
+
+// for(let i=arr.length-1;i>0;i--)
+// {
+//     console.log(arr[i]);
+// }
+
+// let i=0;
+// while (i<arr.length) 
+// {
+//     console.log(arr[i]);    
+//     i++;
+// }
+
+
+
+
+
+
