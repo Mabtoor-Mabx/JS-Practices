2fdd10a31cd56119b719638d5e88dda89744e1c5
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-03-29 23:40:54 -0700

    Add files via upload
    Done

---
+++ DOM Practices/app.js
@@ -0,0 +1,155 @@
+function heading()
+{
+    let head = document.getElementById('head');
+    head.style.color = 'Blue';
+    head.style.textAlign = 'center';
+}
+function Out()
+{
+    let head = document.getElementById('head');
+    head.style.color = 'black';
+    head.style.textAlign = 'left';
+}
+
+function lorem()
+{
+    let lorem1 = document.getElementById('lorem');
+    lorem1.style.color = 'orange';
+    lorem1.style.fontSize = '25px';
+    lorem1.style.fontWeight = 'bold';
+    lorem1.style.border = '1 px solid black';
+}
+function loremout()
+{
+    let lorem1 = document.getElementById('lorem');
+    lorem1.style.color = '';
+    lorem1.style.fontSize = '';
+    lorem1.style.fontWeight = '';
+    lorem1.style.border = '';
+}
+
+
+
+
+
+
+function loremout2()
+{
+    let lorem2 = document.getElementById('para1');
+    lorem2.style.color = '';
+    lorem2.style.fontSize = '';
+    lorem2.style.fontWeight = '';
+    lorem2.style.border = '';
+
+}
+
+function loremout3()
+{
+    let lorem3 = document.getElementById('cicero');
+    lorem3.style.color = '';
+    lorem3.style.fontSize = '';
+    lorem3.style.fontWeight = '';
+    lorem3.style.border = '';
+
+}
+
+function loremout4()
+{
+    let lorem4 = document.getElementById('para2');
+    lorem4.style.color = '';
+    lorem4.style.fontSize = '';
+    lorem4.style.fontWeight = '';
+    lorem4.style.border = '';
+
+}
+
+
+function loremout5()
+{
+    let lorem5 = document.getElementById('Werther');
+    lorem5.style.color = '';
+    lorem5.style.fontSize = '';
+    lorem5.style.fontWeight = '';
+    lorem5.style.border = '';
+
+}
+
+
+function loremout6()
+{
+    let lorem6 = document.getElementById('para3');
+    lorem6.style.color = '';
+    lorem6.style.fontSize = '';
+    lorem6.style.fontWeight = '';
+    lorem6.style.border = '';
+
+}
+
+
+
+
+
+
+
+
+
+function para1()
+{
+    let font = document.getElementById('para1');
+    font.style.fontFamily = 'arial';
+    font.style.color = 'blue';
+    font.style.fontSize = '25px';
+}
+function Readmore()
+{
+    let fulltext = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,';
+    let para = document.getElementById('para1');
+    para.innerHTML = fulltext;
+    
+}
+function para2()
+{
+    let lorem1 = document.getElementById('cicero');
+    lorem1.style.color = 'pink';
+    lorem1.style.fontSize = '27px';
+    lorem1.style.fontWeight = 'bolder';
+    lorem1.style.border = '2 px solid black';
+}
+function para3()
+{
+    let font = document.getElementById('para2');
+    font.style.fontFamily = 'arial';
+    font.style.color = 'blue';
+    font.style.fontSize = '25px';
+
+}
+function ReadAgain()
+{
+    let text = 'natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere';
+    let parra = document.getElementById('para2');
+    parra.innerHTML = text;
+}
+function para4()
+{
+    let lorem1 = document.getElementById('Werther');
+    lorem1.style.color = 'purple';
+    lorem1.style.fontSize = '23px';
+    lorem1.style.fontWeight = 'bold';
+    lorem1.style.border = '3 px solid black'
+}
+
+function para5()
+{
+    let lorem1 = document.getElementById('para3');
+    lorem1.style.color = 'green';
+    lorem1.style.fontSize = '20px';
+    lorem1.style.fontWeight = 'bold';
+    lorem1.style.border = '2 px solid black'
+}
+
+function AgainRead()
+{
+    let text = 'my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath';
+    let para = document.getElementById('para3');
+    para.innerHTML = text;
+}
