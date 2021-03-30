255dbe62117c61a95f9dde5a69f7e4b01b734a2e
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-03-30 02:40:25 -0700

    Add files via upload
    Done

---
+++ Bulb/bulb.js
@@ -0,0 +1,10 @@
+function powerOff()
+{
+    let image = document.getElementById('image');
+    image.src = 'off.jpg'
+}
+function powerOn()
+{
+    let image = document.getElementById('image');
+    image.src = 'on.jpg'
+}
