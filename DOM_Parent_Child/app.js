4f66250b1d1bcd2f128d373a7a266b0a92e890ac
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-04-02 11:16:53 +0530

    Add files via upload
    Done

---
+++ DOM_Parent_Child/app.js
@@ -0,0 +1,42 @@
+// let all = document.getElementById('all');
+//  let aaa=  all.getAttribute('lang');
+// console.log(aaa);
+
+ 
+
+
+
+// let all = document.getElementById('all').attributes;
+// console.log(all);
+
+// let all = document.getElementById('all').parentNode.parentNode.parentNode;
+// console.log(all);
+
+
+
+
+
+// let p = document.createElement('p');
+// let attr = p.setAttribute('class', 'Paragraph');
+// let text = document.createTextNode("Hello World");
+// p.appendChild(text);
+// console.log(p);
+
+
+
+
+// let img = document.createElement('img');
+// let attr = img.setAttribute('class', 'images');
+
+// console.log(img);
+
+
+
+
+
+// let main = document.getElementById('main');
+// let p = document.createElement('p');
+// let text = document.createTextNode('Hello World');
+// p.appendChild(text);
+// main.appendChild(p);
+
